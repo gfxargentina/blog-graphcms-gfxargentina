99ce1d3b62f1cbf2796a08199303484039338310
Luis Chilo <gfxargentina@gmail.com>
2022-04-18 16:57:42 -0300

    add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,34 @@ export default function Home() {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
-        <title>Gfxargentina</title>
+        //Primary Meta Tags
+        <title>GFXARGENTINA</title>
+        <meta name="title" content="GFXARGENTINA" />
+        <meta
+          name="description"
+          content="Programacion - Diseño - Audio - Video"
+        />
+        // Open Graph / Facebook
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.gfxargentina.com.ar/" />
+        <meta property="og:title" content="GFXARGENTINA" />
+        <meta
+          property="og:description"
+          content="Programacion - Diseño - Audio - Video"
+        />
+        <meta property="og:image" content="/GFXARGENTINA-META.PNG" />
+        //Twitter
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://www.gfxargentina.com.ar/"
+        />
+        <meta property="twitter:title" content="GFXARGENTINA" />
+        <meta
+          property="twitter:description"
+          content="Programacion - Diseño - Audio - Video"
+        />
+        <meta property="twitter:image" content="/GFXARGENTINA-META.PNG" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
