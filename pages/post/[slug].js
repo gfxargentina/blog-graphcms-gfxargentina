eb6abb170ca8bca8cc68bb32a77da5d32635cbe4
Luis Chilo <gfxargentina@gmail.com>
2022-04-26 22:08:05 -0300

    add dynamic post metatags seo

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -8,11 +8,28 @@ import {
   Comments,
   CommentsForm,
 } from '../../components'
+import Head from 'next/head'
 
 const PostDetails = ({ post }) => {
-  console.log(post)
   return (
     <div className="container mx-auto mb-8 px-10">
+      <Head>
+        <title>{post.title}</title>
+        <meta name="title" content={post.title} />
+        <meta name="description" content={post.excerpt} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={post.slug} />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.excerpt} />
+        <meta property="og:image" content={post.featuredImage.url} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={post.slug} />
+        <meta property="twitter:title" content={post.title} />
+        <meta property="twitter:description" content={post.excerpt} />
+        <meta property="twitter:image" content={post.featuredImage.url} />
+      </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post} />
