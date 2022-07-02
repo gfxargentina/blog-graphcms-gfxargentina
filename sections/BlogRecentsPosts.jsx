ffe3932283cfefe0aefdf6a0b90b19d480a9b800
Luis Chilo <gfxargentina@gmail.com>
2022-07-02 11:25:07 -0300

    add revalidate to post slug page

--- sections/BlogRecentsPosts.jsx
+++ sections/BlogRecentsPosts.jsx
@@ -10,7 +10,6 @@ const BlogRecentsPosts = () => {
       setRecentPosts(result)
     })
   }, [])
-  console.log(recentPosts)
 
   return (
     <div className="container mx-auto   items-center  px-8 md:px-14 lg:px-24">
