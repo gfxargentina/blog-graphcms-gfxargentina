b2abff28f66a81e8695d8a1b6fb6f54ccfa9c566
Luis Chilo <gfxargentina@gmail.com>
2022-07-02 15:41:35 -0300

    back to initial state

--- sections/BlogRecentsPosts.jsx
+++ sections/BlogRecentsPosts.jsx
@@ -1,43 +1,41 @@
 import React, { useEffect, useState } from 'react'
-import RecentsPosts from '../sections/RecentPosts'
-//import { getRecentPosts } from '../services'
+import { getRecentPosts } from '../services'
 
 const BlogRecentsPosts = () => {
-  return <RecentsPosts />
-  // const [recentPosts, setRecentPosts] = useState([])
+  const [recentPosts, setRecentPosts] = useState([])
 
-  // useEffect(() => {
-  //   getRecentPosts().then((result) => {
-  //     setRecentPosts(result)
-  //   })
-  // }, [])
+  useEffect(() => {
+    getRecentPosts().then((result) => {
+      setRecentPosts(result)
+    })
+  }, [])
 
-  // return (
-  //   <div className="container mx-auto   items-center  px-8 md:px-14 lg:px-24">
-  //     <section className="">
-  //       <h2 className="mt-5 mb-5 text-2xl text-lime-400">
-  //         Ultimos Posts del Blog
-  //       </h2>
+  return (
+    <div className="container mx-auto   items-center  px-8 md:px-14 lg:px-24">
+      <section className="">
+        <h2 className="mt-5 mb-5 text-2xl text-lime-400">
+          Ultimos Posts del Blog
+        </h2>
 
-  //       {/* <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-  //         {recentPosts.map((post) => (
-  //           <RecentPostCard post={post} key={post.title} />
-  //         ))}
-  //       </div> */}
-  //       <div className="">
-  //         <div className="">
-  //           <div className="xs:grid-cols-2 grid  gap-4 md:grid md:grid-cols-2 lg:grid-cols-3">
-  //             {recentPosts.map((post) => (
-  //               <div key={post.title}>
-  //                 <RecentPostCard post={post} />
-  //               </div>
-  //             ))}
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </section>
-  //   </div>
-  // )
+        {/* <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+           {recentPosts.map((post) => (
+             <RecentPostCard post={post} key={post.title} />
+           ))}
+         </div> */}
+        <div className="">
+          <div className="">
+            <div className="xs:grid-cols-2 grid  gap-4 md:grid md:grid-cols-2 lg:grid-cols-3">
+              {recentPosts.map((post) => (
+                <div key={post.title}>
+                  <RecentPostCard post={post} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default BlogRecentsPosts
